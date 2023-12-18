b00851c9d1910d5209e798c58467b917175e02ad
L1KEAB0T <wjy1370858716@gmail.com>
2023-12-18 21:51:55 +0800

    改一下

--- 电网造价/电网造价.cpp
+++ 电网造价/电网造价.cpp
@@ -3,20 +3,23 @@
 const int MAX = 26; // 最大小区数
 
 // 定义边的结构体
+template <typename T>
 struct Edge {
-    char start;
-    char end;
+    T start;
+    T end;
     int cost;
 };
 
 // 定义并查集
+template <typename T>
 struct DisjointSet {
-    int parent[MAX];
+    T parent[MAX];
     int rank[MAX];
 };
 
 // 初始化并查集
-void makeSet(DisjointSet& ds, int n) {
+template <typename T>
+void makeSet(DisjointSet<T>& ds, int n) {
     for (int i = 0; i < n; i++) {
         ds.parent[i] = i;
         ds.rank[i] = 0;
@@ -24,16 +27,18 @@ void makeSet(DisjointSet& ds, int n) {
 }
 
 // 查找根节点
-int find(DisjointSet& ds, int i) {
+template <typename T>
+T find(DisjointSet<T>& ds, T i) {
     if (i != ds.parent[i])
         ds.parent[i] = find(ds, ds.parent[i]);
     return ds.parent[i];
 }
 
 // 合并两个集合
-void Union(DisjointSet& ds, int x, int y) {
-    int rootX = find(ds, x);
-    int rootY = find(ds, y);
+template <typename T>
+void Union(DisjointSet<T>& ds, T x, T y) {
+    T rootX = find(ds, x);
+    T rootY = find(ds, y);
 
     if (ds.rank[rootX] < ds.rank[rootY])
         ds.parent[rootX] = rootY;
@@ -46,11 +51,12 @@ void Union(DisjointSet& ds, int x, int y) {
 }
 
 // 冒泡排序边
-void sortEdges(Edge edges[], int n) {
+template <typename T>
+void sortEdges(Edge<T> edges[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (edges[j].cost > edges[j + 1].cost) {
-                Edge temp = edges[j];
+                Edge<T> temp = edges[j];
                 edges[j] = edges[j + 1];
                 edges[j + 1] = temp;
             }
@@ -59,19 +65,20 @@ void sortEdges(Edge edges[], int n) {
 }
 
 // Kruskal算法构造最小生成树
-void kruskal(Edge edges[], int n, int e) {
-    DisjointSet ds;
+template <typename T>
+void kruskal(Edge<T> edges[], int n, int e) {
+    DisjointSet<T> ds;
     makeSet(ds, n);
 
     const int resultSize = n - 1;
-    Edge result[MAX];
+    Edge<T> result[MAX];
     int mstCost = 0;
     int i = 0;
 
     while (i < resultSize && e < n * (n - 1) / 2) {
-        Edge nextEdge = edges[e++];
-        int x = find(ds, nextEdge.start - 'a');
-        int y = find(ds, nextEdge.end - 'a');
+        Edge<T> nextEdge = edges[e++];
+        T x = find(ds, nextEdge.start);
+        T y = find(ds, nextEdge.end);
 
         if (x != y) {
             result[i++] = nextEdge;
@@ -93,7 +100,7 @@ int main() {
 
     e = 0;
     const int maxEdges = n * (n - 1) / 2;
-    Edge edges[MAX];
+    Edge<char> edges[MAX];
 
     std::cout << "请输入小区之间的电网边数: ";
     int edgeCount;
